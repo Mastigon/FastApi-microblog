2b0ec126fba03f387ec09d177d8c297592061722
Igores <devigorek@gmail.com>
2022-09-13 19:50:56 +0300

    add routes post_list and create_post

--- microblog/models.py
+++ microblog/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String, Integer, Text, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from core.db import Base
+from user.models import User
+
 
 class Post(Base):
     __tablename__ = "microblog_posts"
@@ -9,4 +11,4 @@ class Post(Base):
     text = Column(Text)
     date = Column(DateTime)
     user = Column(Integer, ForeignKey("user.id"))
-    user_id = relationship("User")
+    user_id = relationship(User)
